381dd3f1f4d428ee9c86a200c5ff96b97d081cb5
DagoGres <dan333pro@gmai.com>
2023-09-26 14:48:12 -0500

    Cifrar contraseñas

--- api/components/auth/controller.js
+++ api/components/auth/controller.js
@@ -1,5 +1,6 @@
 const TABLA = "auth";
 const auth = require('../../../auth/index');
+const bcrypt = require('bcrypt');
 
 module.exports = function (injectedStore) {
     let store = injectedStore;
@@ -9,15 +10,18 @@ module.exports = function (injectedStore) {
 
     async function login(username, password) {
         const data = await store.query(TABLA, { username: username});
-        if (data.password === password) {
-            // Generar Token
-            return auth.sign(data);
-        } else {
-            throw new Error('Información invalida');
-        }
+        return bcrypt.compare(password, data.password)
+        .then(sonIguales => {
+            if (sonIguales) {
+                // Generar Token
+                return auth.sign(data);
+            } else {
+                throw new Error('Información invalida');
+            }
+        });
     }
 
-    function upsert(data) {
+    async function upsert(data) {
         const authData = {
             id: data.id,
         };
@@ -26,7 +30,7 @@ module.exports = function (injectedStore) {
             authData.username = data.username;
         }
         if (data.password) {
-            authData.password = data.password;
+            authData.password = await bcrypt.hash(data.password, 5);
         }
 
         return store.upsert(TABLA, authData);
