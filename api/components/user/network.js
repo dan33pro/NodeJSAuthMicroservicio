0f969d42543c9fdbc34c277caabc99639a3f28ce
DagoGres <dan333pro@gmai.com>
2023-09-25 20:42:46 -0500

    Estructura inicial del proyecto: API y rutas

---
+++ api/components/user/network.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const response = require('../../../network/response')
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    response.success(req, res, 'Todo correcto', 200);
+});
+
+module.exports = router;
+
